14ce83813603679fff9e26bf5a2fdcc196d62fc5
jmmuller <jean-michael.muller@ign.fr>
2026-01-30 09:13:53 +0100

    Accept empty patterns for OriBundleAdj, SimulOriPerturbRandom and ReportPoseCmp

--- src/Appli/cMMVII_Appli.cpp
+++ src/Appli/cMMVII_Appli.cpp
@@ -820,7 +820,7 @@ void cMMVII_Appli::InitParam(cGenArgsSpecContext *aArgsSpecs)
                const std::string & aNameInterval = mIntervFilterMS[aNum];
                if (IsInit(&aNameInterval))
                {
-                       // JOE
+                   // JOE
                    cPatternTransfo aPat(mTransfoFFI[aNum]);
                    mVMainSets.at(aNum).Filter(Str2Interv<std::string>(aNameInterval),aPat);
                    // mVMainSets.at(aNum).Filter(Str2Interv<std::string>(aNameInterval));
@@ -832,11 +832,11 @@ void cMMVII_Appli::InitParam(cGenArgsSpecContext *aArgsSpecs)
                 // if we are in a recall mode, posibly pattern comes file xml, and file insid can be un-existent
                 if (mLevelCall>0)
                 {
-                   mVMainSets.at(aNum).Add(aVValues[aK]);
+                    mVMainSets.at(aNum).Add(aVValues[aK]);
                 }
                 else
                 {
-                   MMVII_UserError(eTyUEr::eEmptyPattern,"Specified set of files was empty");
+                    MMVII_UserError(eTyUEr::eEmptyPattern,"Specified set of files was empty");
                 }
             } else if (mVMainSets.at(aNum).size() == 1){
                 // If only 1 file matches the pattern, we replace the pattern in this arg by the matched file
