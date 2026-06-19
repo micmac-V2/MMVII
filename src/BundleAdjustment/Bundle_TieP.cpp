360416b63fd3414f9a07fd9a1e275ad3f634a26c
deseilligny <marc.pierrot-deseilligny@ign.fr>
2026-06-19 16:42:18 +0200

    Push back UnWeighted Residual in TieP/BA

--- src/BundleAdjustment/Bundle_TieP.cpp
+++ src/BundleAdjustment/Bundle_TieP.cpp
@@ -41,6 +41,7 @@ void cMMVII_BundleAdj::OneItere_TieP(const cBA_TieP& aBA_TieP)
    aMTP->SetPGround();
 
    cWeightAv<tREAL8> aWeigthedRes;
+   cWeightAv<tREAL8> aUnWeightedRed; ///< MPD : a conserver, ils ont mysterieusement disparus; mais il sont pourtant tres utiles
    for (const auto & aPair : aMTP->Pts())
    {
        const auto & aConfig  = aPair.first;
@@ -93,12 +94,14 @@ void cMMVII_BundleAdj::OneItere_TieP(const cBA_TieP& aBA_TieP)
                       anObj->PushIndexes(aVIndGlob);
                    }
 
+                   tREAL8 aN2Res = Norm2(aResidual);
                    if (aWeightImage>0)
                    {
-                       aWeigthedRes.Add(aWeightImage,Norm2(aResidual));
+                       aWeigthedRes.Add(aWeightImage,aN2Res);
                        mSys->R_AddEq2Subst(aStrSubst,anEqColin,aVIndGlob,aVObs,aWeightImage);
                        aNbEqAdded++;
                    }
+                   aUnWeightedRed.Add(1.0,aN2Res);
                }
            }
 
@@ -109,7 +112,9 @@ void cMMVII_BundleAdj::OneItere_TieP(const cBA_TieP& aBA_TieP)
 
    }
    StdOut() <<  "  # " << aBA_TieP.mName << ": Weighted Residual=" << aWeigthedRes.Average()
-            << " (" << aWeigthedRes.Nb() << " obs)" << std::endl;
+            << " (" << aWeigthedRes.Nb() << " obs)" 
+            << " UnWeitghRes=" << aUnWeightedRed.Average()
+            << std::endl;
 }
 
 void cMMVII_BundleAdj::OneItere_TieP()
